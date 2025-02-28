c0e05e07d750e18ed91afadd6fa12b5d3c5146f2
AndreTells <andresilvatelles@gmail.com>
2025-02-28 09:46:53 +0100

    implementation of ressource database

---
+++ src/ressource_database.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include <stdlib.h>
+
+#include "ressource_database.h"
+
+RessourceDataBase_t * init_db(){
+  RessourceDataBase_t * db = (RessourceDataBase_t *) malloc(sizeof(RessourceDataBase_t));
+  memset(db->registered,0,MAX_RESSOURCE_ID);
+  memset(db->availability,0,MAX_RESSOURCE_ID);
+  return db;
+}
+
+int attemptLockRessource(RessourceDataBase_t * db, int ressourceId){
+  // if ressource is not registered, register it
+  if(db.registered[ressourceId]){
+    db.registered[ressourceId] = 1;
+    db.availability[ressourceId] = 1;
+  }
+
+  // check if ressource is available
+  if(db.availability[ressourceId]){
+    // lock ressource
+    db.availability[ressourceId] -= 1;
+    return 1;
+  }
+
+  // ressource was not available
+  return 0;
+}
+
+int releaseRessourceById(RessourceDataBase_t * db, int ressourceId){
+  // if ressource is not registered, return error
+  if(!db.registered[ressourceId]){
+    return -1;
+  }
+
+  // check if ressource is available
+  db.availability[ressourceId] = 1;
+
+  // ressource was not available
+  return 1;
+}

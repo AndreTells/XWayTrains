c920b25c36714bf4e3354c787bc5bd4b162ec805
AndreTells <andresilvatelles@gmail.com>
2025-03-24 13:41:42 +0100

    corrected compatibility issues with remote test plc

--- src/remote_test/remote_test_plc.c
+++ src/remote_test/remote_test_plc.c
@@ -7,7 +7,7 @@
 #include "common/flags.h"
 #include "common/verbose.h"
 
-#define HOST_ADDR "127.0.0.1"
+#define HOST_ADDR "172.31.71.25"
 #define SERVER_ADDR "10.31.125.14"
 #define PLC_PORT 502
 
@@ -37,33 +37,35 @@ int main(int argc, char* argv[]){
   char* train_s = "1";
   (void)get_flag_value(argc,argv,"--train",&train_s);
   int train = atoi(train_s);
+  verbose("[PLC TEST CLIENT]: targeting train %d \n", train);
 
   char* target_s = "20";
   (void)get_flag_value(argc,argv,"--target",&train_s);
   int target = atoi(target_s);
+  verbose("[PLC TEST CLIENT]: targeting rail %d \n", train);
 
   // sending out messages
   PlcProxy_t* proxy = initPlcProxy(HOST_ADDR, SERVER_ADDR, PLC_PORT);
-  assert(proxy != NULL);
 
-  /* making a message */
-  PlcMessage_t* msg = createPlcMessage();
-  assert(msg != NULL);
-  int res;
   uint8_t pc_station = 0x28;
   uint8_t plc_station = 0x0E;
 
   uint8_t network = 1;
   uint8_t port = 0;
 
-   res = configWritePlcMessage(msg, TOGGLE_SWITCH, pc_station , TRAIN_1, SWITCH_GROUP_31);
-  assert(res == 0);
-
   /* configure the address information */
   XwayAddr sender = createXwayAddr(pc_station, network, port);
   XwayAddr receiver = createXwayAddr(plc_station, network, port);
-  uint8_t extAddr[2] = {0x09, 0x10};
-  res = setNPDU(msg, NPDU_5WAY, sender, receiver, extAddr);
+  setXwayAddrs(proxy,  pc_station, plc_station, network, port);
+
+  assert(proxy != NULL);
+
+  /* making a message */
+  PlcMessage_t* msg = createPlcMessage();
+  assert(msg != NULL);
+  int res;
+
+   res = configWritePlcMessage(msg, TOGGLE_RAIL, pc_station , train, target);
   assert(res == 0);
 
   // attempting to send the message

75f4c7737265ede318f62cd31f89048b5e086a3d
AndreTells <andresilvatelles@gmail.com>
2025-03-25 18:20:11 +0100

    prototype for routes 2 and 3 done and address of trains 3 and 4 corrected

--- src/plc/model_info.c
+++ src/plc/model_info.c
@@ -6,8 +6,8 @@
 static const uint16_t TrainAddrDict[] = {
     [TRAIN_1] = 0x0027,
     [TRAIN_2] = 0x002A,
-    [TRAIN_3] = 0x0032,
-    [TRAIN_4] = 0x0035,
+    [TRAIN_3] = 0x0031,
+    [TRAIN_4] = 0x0034,
 };
 
 uint16_t getTrainAddr(TrainId_e trainId) { return TrainAddrDict[trainId]; }
